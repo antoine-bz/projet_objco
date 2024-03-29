0aedfd3ac2d5484566a461c4b857fff2543da1ff
antoine siuuuuuuuuu <anboez@hotmail.fr>
2024-03-29 09:59:31 +0100

    ajout LIBVLC

--- setup-vlc.sh
+++ setup-vlc.sh
@@ -5,10 +5,24 @@ export PATH2=$(pwd)
 export PATH_CC=$PATH2/tools-master/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin
 export LD_LIBRARY_PATH=$PATH2/target_vlc/lib:$LD_LIBRARY_PATH
 export CCC=$PATH_CC/arm-linux-gnueabihf-gcc
-export CC=$CCC
-export CXX=$PATH_CC/arm-linux-gnueabihf-g++
+#export CC=$CCC
+#export CXX=$PATH_CC/arm-linux-gnueabihf-g++
 
-cd vlc-3.0.9.2
+TRIPLET=$PATH_CC/arm-linux-gnueabihf
+
+
+export CC=$TRIPLET-gcc
+export CXX=$TRIPLET-g++
+export CPP=$TRIPLET-cpp
+export AR=$TRIPLET-ar
+export RANLIB=$TRIPLET-ranlib
+export ADD2LINE=$TRIPLET-addr2line
+export AS=$TRIPLET-as
+export LD=$TRIPLET-ld
+export NM=$TRIPLET-nm
+export STRIP=$TRIPLET-strip
+
+cd vlc-master
 
 make clean
 
